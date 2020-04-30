eacc717c0ba3b19e5784f9fe6a74aeeea02dc106
Mitch Webster <mitch83083@gmail.com>
2020-04-29 20:09:02 -0400

    Basic graphQL queries and server

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const { db } = require('./database/pgAdaptor')
+const graphql = require('graphql')
+const express = require('express')
+const expressGraphQl = require('express-graphql')
+const { GraphQLSchema } = graphql
+const { query } = require('./database/schemas/query')
+const { mutation } = require('./database/schemas/mutation')
 
-db.one('select * from users').then((res) => {
-  console.log(res)
+const schema = new GraphQLSchema({
+  query,
+  mutation
 })
+
+const app = express()
+
+app.use(
+  '/',
+  expressGraphQl({
+    schema: schema,
+    graphiql: true
+  })
+)
+
+app.listen(5000, () => console.log('GraphQL server running on localhost:5000'))
